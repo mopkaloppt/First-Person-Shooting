3cfba4d6eb42fcb028dbb6cbaf49290a54fd87f5
mopkaloppt <moppi.sgotmoney@gmail.com>
2022-06-12 10:42:15 +0200

    Initial commit

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] float damage = 30f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("I hit " + hit.transform.name);
+            // TO DO: add some hit effect for visual players
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }       
+    }
+}
