3ad1416c645f9d247d103bafa06b225d81933610
mopkaloppt <moppi.sgotmoney@gmail.com>
2022-06-16 22:51:18 +0200

    Add Weapon, Ammon, DeathHandler

---
+++ Assets/Scripts/DeathHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathHandler : MonoBehaviour
+{
+    [SerializeField] Canvas gameOverCanvas;
+
+    private void Start()
+    {
+        gameOverCanvas.enabled = false;
+    }
+    
+    public void HandleDeath()
+    {
+        gameOverCanvas.enabled = true;
+        // Must stop the time so the player can have a cursor back smoothly
+        Time.timeScale = 0;
+        // Don't lock the cursor
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;      
+    }
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -11,7 +11,7 @@ public class PlayerHealth : MonoBehaviour
         hitPoints -= damage;
         if (hitPoints <= 0)
         {
-           Debug.Log("YOU DEAD!!!");
+           GetComponent<DeathHandler>().HandleDeath();
         }
     }
 }
